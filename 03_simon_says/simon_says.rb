c534fa6e9f3a49aa78604c6490d05d91172264f7
Loc Nguyen <nguyenloc300796@gmail.com>
2017-05-10 19:29:20 +0000

    Complete Exercises 3 and 4

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,27 @@
 #write your code here
+def echo(string)
+    string
+end
+
+def shout(string)
+    string.upcase
+end
+
+def repeat(string, reps = 2)
+    arr = []
+    reps.times { arr << string }
+    arr.join(" ")
+end
+
+def start_of_word(string, pos)
+    string[0..pos - 1]
+end
+
+def first_word(string)
+    string.split(" ")[0]
+end
+
+def titleize(string)
+    little_words = ["a", "an", "the", "at", "by", "for", "in", "of", "on", "over", "to", "up", "and", "as", "but", "or", "nor"]
+    string.capitalize.split.collect { |word| little_words.include?(word)? word : word.capitalize }.join(" ")
+end
--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,4 @@
 #write your code here
+def translate(string)
+    string.split.collect { |word| word[word.index(/((?<![Qq])([uU])|[aeioAEIO])/)..-1] + word[0, word.index(/((?<![Qq])([uU])|[aeioAEIO])/)] + "ay"}.join(" ")
+end
